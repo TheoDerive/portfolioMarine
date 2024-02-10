7c5c0adfd295c109ba9fb8f526e28cf96686e9ef
TheoDerive <theo.derive.pro@gmail.com>
2024-02-10 10:04:58 +0000

    Trying something

--- app/api/all-categories/route.js
+++ app/api/all-categories/route.js
@@ -2,6 +2,8 @@ import { connectToDB } from "@/utils/database";
 import Category from "@/models/Category";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req, res) {
   await connectToDB();
 
